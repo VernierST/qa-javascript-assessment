f95d95f1e2231513e95b5c5972ea0cce01a5ed0c
SusanBird <susanbird391@gmail.com>
2022-10-16 21:49:42 -0700

    mapped out process, trying to figure out proper waitUntil syntax

--- test/vernier-app.test.js
+++ test/vernier-app.test.js
@@ -1,5 +1,5 @@
 import { html } from 'lit';
-import { fixture, expect, waitUntil } from '@open-wc/testing';
+import { fixture, expect } from '@open-wc/testing';
 
 import '../src/vernier-app.js';
 
@@ -17,9 +17,32 @@ describe('VernierApp', () => {
 
   it('renders a list of 15 natural event titles', async () => {
     // Complete this test
+
+    const ordList = element.shadowRoot.querySelector('ol');
+    expect(ordList).to.exist;
   });
+
+  // it('renders a p tag', () => {
+  //   const p = element.shadowRoot.querySelector('p');
+  //   expect(p).to.exist;
+  //   expect(p.textContent).to.contain('Thanks');
+  // });
 
   it('passes the a11y audit', async () => {
     await expect(element).shadowDom.to.be.accessible();
   });
 });
+
+// how to show there are 15 natural event titles?
+// 1. check that data gets through at all
+// 2. check that there is a list
+// 3. check that there are 15 <li></li>
+// 4. check that the <li></li> contain strings
+// 5. check that each string contains a natural event word
+//   -- but what to do with the one in French?
+
+// I've used superagent in the past, so need to figure out the syntax of the jest requests without superagent - find another way to make requests to the app
+// shadowRoot explanation here: https://www.geeksforgeeks.org/what-is-shadow-root-and-how-to-use-it/
+// had trouble targeting the list, it was populating on the site but the test couldn't target it, changed syntax all around
+// cant target the ol or the p tag with the same shadowRoot.querySelector syntax - what makes the h1 different, why can it target that? it could possibly be that I'm now trying to target a "lit" element and it is still being built?
+// need to use waitUntil to give enough time for list to render before testing for it
